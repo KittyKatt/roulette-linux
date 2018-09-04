fa24735c1671b4e1ea08f3f9c171c1632f882590
loop-de-r00t <r00t@spotchat.org>
2018-09-04 01:14:45 -0400

    "Revert" back to original fix, and reset tally_loc to 0 after a bad command
    Now we only have a single file to worry about surviving reboots

--- roulette.sh
+++ roulette.sh
@@ -18,35 +18,37 @@ declare -a command_list=(
   "users"
 )
 # Set this to the full path to tally.txt
-tally_loc=/tmp/tally
-wipe_loc=/tmp/wipes
+tally_loc=/tmp/tally.txt
 
 # Create files if not already created
-if [ ! -e $tally_loc ]; then touch $tally_loc && echo "0" > $tally_loc; fi
+if [ ! -e $tally_loc ]; then touch $tally_loc && printf "0\n0\n" > $tally_loc; fi
 
 #Initialize varibles
-commandS_num=$(cat $tally_loc)
-wipe_num=$(cat $wipe_loc)
-[ -z ${wipe_num} ] && wipe_num="0"
+commandS_num=$(sed "1q;d" $tally_loc)
+wipe_num=$(sed "2q;d" $tally_loc)
 startTime=$(cat /tmp/starttime)
 nowTime=$(date +'%Y%m%d%H%M%S')
 current_up=$(dateutils.ddiff -f '%d days, %H hours, %M minutes, %S seconds' -i '%Y%m%d%H%M%S' $startTime $nowTime)
 
+# Use shuf to pick random command from the list of possible commands
+#command="$(shuf -n1 ${command_list[@]})"
 # Get random array element
 rand=$[$RANDOM % ${#command_list[@]}]
 
 if ${command_list[$rand]}; then
   ((commandS_num++))
-  echo $commandS_num > $tally_loc
-  [ -f /var/www/html/index.html ] && rm /var/www/html/index.html
+  sed -i "1s/.*/$commandS_num/" $tally_loc
+  rm /var/www/html/index.html
   echo "<h1>Welcome to Suicide Linux Russian Roulette!</h1>" > /var/www/html/index.html
   echo "<br /><ul>" >> /var/www/html/index.html
   echo "<li>Current system uptime: $current_up</li>" >> /var/www/html/index.html
-  echo "<li>Current number of successful (correct) commands: $commandS_num</li>" >> /var/www/html/index.html
-  echo "<li>Current number of times the system has been wiped: $wipe_num</li>" >> /var/www/html/index.html
+  echo "<li>Current number of successful (correct) commands: $(sed '1q;d' $tally_loc)</li>" >> /var/www/html/index.html
+  echo "<li>Current number times the system has been wiped: $(sed '2q;d' $tally_loc)</li>" >> /var/www/html/index.html
   echo "</ul>" >> /var/www/html/index.html
 else
   ((wipe_num++))
-  echo $wipe_num > $wipe_loc
-  kill $(pgrep nginx);
+  sed -i "2s/.*/$wipe_num/" $tally_loc
+  sed -i "1s/.*/0/" $tally_loc
+  # The command from above was bad. Killing the container by killing the webserver.
+  kill -9 $(pgrep nginx);
 fi
